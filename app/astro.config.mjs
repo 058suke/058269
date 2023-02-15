3e45527291b8db6c0bb929fcbf750a9546ab67bf
KOBAYASHI Kosuke <058suke@MacBook-Air058.local>
2023-02-15 23:47:13 +0900

    feat: テンプレートを変更

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,15 +1,36 @@
-import { defineConfig } from 'astro/config';
-import { settings } from './src/data/settings';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import remarkToc from "remark-toc";
+import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: settings.site,
-  integrations: [sitemap(), mdx()],
-  vite: {
-    ssr: {
-      external: ["svgo"]
-    }
-  }
+  site: "https://astro-paper.pages.dev/",
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    react(),
+    sitemap(),
+  ],
+  markdown: {
+    remarkPlugins: [
+      remarkToc,
+      [
+        remarkCollapse,
+        {
+          test: "Table of contents",
+        },
+      ],
+    ],
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true,
+    },
+    extendDefaultPlugins: true,
+  },
 });
