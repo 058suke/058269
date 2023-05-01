5adf46f9f6e237f55a2d12a144898b1888165a63
KOBAYASHI Kosuke <058suke@MacBook-Air058.local>
2023-05-01 11:50:24 +0900

    feat: npmパッケージアップデート

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import remarkToc from "remark-toc";
-import remarkCollapse from "remark-collapse";
+import rehypeToc from "@jsdevtools/rehype-toc";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
@@ -18,14 +19,50 @@ export default defineConfig({
     sitemap(),
   ],
   markdown: {
-    remarkPlugins: [
-      remarkToc,
+    remarkPlugins: [],
+    rehypePlugins: [
+      rehypeSlug,
       [
-        remarkCollapse,
+        rehypeAutolinkHeadings,
         {
-          test: "Table of contents",
-        },
+          behavior: 'prepend',
+          properties: {
+            className: ['anchor'],
+            ariaHidden: true, 
+            tabIndex: -1
+          },
+          content: {
+            type: 'element',
+            tagName: 'span',
+            properties: {
+              className: ['octicon', 'octicon-link']
+            }
+          }
+        }
       ],
+      [
+        rehypeToc,
+        {
+          headings: ["h2", "h3"],
+          cssClasses: {
+            toc: "post-toc",
+            list: "post-toc-list"
+          },
+          customizeTOC: (toc) => {
+            const replacer = (children) => {
+              children.forEach(child => {
+                if(child.type === 'element' && child.tagName === 'ol') {
+                  child.tagName = 'ul';
+                }
+                if(child.children) {
+                  replacer(child.children);
+                }
+              });
+            };
+            replacer([toc]);
+          }
+        }
+      ]
     ],
     shikiConfig: {
       theme: "one-dark-pro",
